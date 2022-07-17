57866c4aacf70758d2fdcec591be11d2366c8a8c
DenicoP <denicopush@gmail.com>
2022-07-17 17:31:35 +0300

    lesson on questions

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const numberOffFilms = +prompt('Сколько фильмов Вы уже просмотрели', '');
+
+const personalMovieDB = {
+    count: numberOffFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const a = prompt('Один из последних просмотреных фильмов?', ''),
+      b = prompt('Насколько оцениваете его?', ''),
+      c = prompt('Один из последних просмотреных фильмов?', ''),
+      d = prompt('Насколько оцениваете его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
